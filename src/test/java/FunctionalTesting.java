55dbdb362c6d1fa3ff209fd7dc58e375e60d0691
Taygib <nfsmwm5@mail.ru>
2023-06-06 01:23:21 +0300

    FunctionalTesting

--- src/test/java/FunctionalTesting.java
+++ src/test/java/FunctionalTesting.java
@@ -1,6 +1,9 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -17,14 +20,60 @@ public class FunctionalTesting {
     void firstThreeButtonsOrderService() { // заказать услугу кнопка
 
         open("https://www.performance-lab.ru/funkcionalnoe-testirovanie");
-        $("#masthead").find(byText("Заказать услугу")).click();
-        $(".premium-modal-box-modal-body").find(byText("Имя")).hover();
-        $(".wpcf7-form-control").click();
-        $(".wpcf7-form-control").setValue("граф");
 
-        $(".premium-modal-box-modal-body").find(byText("Фамилия")).click();
-        // $(".wpcf7-form-control").click();
-        // $(".wpcf7-form-control").setValue("краснов");
+        // заказать услугу в правом верхнем углу
+        $("#masthead").find(byText("Заказать услугу")).click();
+
+        $(".premium-modal-box-modal-title").shouldHave(text("Свяжитесь с нами"));
+
+        $(".premium-modal-box-modal-body").shouldHave(text("Имя"));
+        $("[name=your-name]").click();
+        $("[name=your-name]").setValue("граф");
+
+        $(".premium-modal-box-modal-body").shouldHave(text("Фамилия"));
+        $("[name=your-surname]").click();
+        $("[name=your-surname]").setValue("краснов");
+
+        $(".premium-modal-box-modal-body").shouldHave(text("Email"));
+        $("[name=your-email]").click();
+        $("[name=your-email]").setValue("hghghg@mail.ru");
+
+        $(".premium-modal-box-modal-body").shouldHave(text("Телефон"));
+        $("[name=your-tel]").click();
+        $("[name=your-tel]").setValue("7777777777");
+
+        $(".premium-modal-box-modal-body").shouldHave(text("Компания"));
+        $("[name=your-organization]").click();
+        $("[name=your-organization]").setValue("FunctionalLab");
+
+        $(".premium-modal-box-modal-body").shouldHave(text("Сообщение"));
+        $("[name=your-message]").click();
+        $("[name=your-message]").setValue("FunctionalTest");
+
+        $("[value=Отправить]").hover();
+
+        $(".premium-modal-box-close-button-container").click();
+
+        // заказать услугу под названием Функциональное тестирование
+
+        $(".col-12").shouldHave(text("Функциональное тестирование"));
+        $(".col-12").shouldHave(text("Сделайте шаг навстречу качеству"));
+
+        $(".testing_btn").find(byText("Заказать услугу")).click();
+
+        //удаление заполненных полей
+        $("[name=your-name]").clear();
+        $("[name=your-surname]").clear();
+        $("[name=your-email]").clear();
+        $("[name=your-tel]").clear();
+        $("[name=your-organization]").clear();
+        $("[name=your-message]").clear();
+
+
+
+
+
+
 
         sleep(4000);
     }
@@ -61,7 +110,7 @@ public class FunctionalTesting {
     }
 
     @Test
-    void examplesOfProjects () { // примеры проектов
+    void examplesOfProjects() { // примеры проектов
 
     }
 }
